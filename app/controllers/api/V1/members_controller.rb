058c762503933904a3392c71f17c0eb13eb3d695
jesalg <jesal7@gmail.com>
2013-09-30 14:05:14 -0700

    Member registration with polymorphic user association

--- app/controllers/api/V1/members_controller.rb
+++ app/controllers/api/V1/members_controller.rb
@@ -10,7 +10,13 @@ class Api::V1::MembersController < ApplicationController
   end
 
   def create
-    respond_with :api, Member.create(member_params)
+    @user = User.new(user_params)
+    @member = Member.new(member_params)
+    @user.role = @member
+    @user.save!
+    @member.user_id = @user.id
+    @member.save!
+    respond_with :api, @member
   end
 
   def update
@@ -25,5 +31,9 @@ class Api::V1::MembersController < ApplicationController
 
   def member_params
     params.require(:member).permit(:first_name, :last_name)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,4 @@
 class Member < ActiveRecord::Base
   has_one :user, as: :role
+  accepts_nested_attributes_for :user
 end
