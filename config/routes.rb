0af960f8bde52d16e0599cc1cc461ac3ab5884dd
jesalg <jesal7@gmail.com>
2013-10-04 11:57:44 -0700

    Using Active Admin instead for backend administration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require 'api_constraints'
 
 Shyne::Application.routes.draw do
+  devise_for :admins, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
