73c2d598e35ded50f65dd3f3206590d781a66eef
jesalg <jesal7@gmail.com>
2013-10-08 11:42:25 -0700

    Adding call and member models to active admin

---
+++ app/admin/call.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register Call do
+  menu :priority => 4
+
+  index do
+    selectable_column
+    column :mentor_id
+    column :member_id
+    column :scheduled_at
+    column :duration
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "Call Details" do
+      f.input :mentor
+      f.input :member
+      f.input :scheduled_at, :as => :just_datetime_picker
+    end
+    f.actions
+  end
+
+  controller do
+    def permitted_params
+      params.permit call: [:mentor_id, :member_id, :scheduled_at, :scheduled_at_date, :scheduled_at_time_hour, :scheduled_at_time_minute]
+    end
+  end
+end
