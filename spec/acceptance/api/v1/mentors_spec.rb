6be4754121bcc7f4299a961a77c9f180fd5fec94
jesalg <jesal7@gmail.com>
2013-10-11 14:34:13 -0700

    A more complete test spec for mentors

--- spec/acceptance/api/v1/mentors_spec.rb
+++ spec/acceptance/api/v1/mentors_spec.rb
@@ -2,16 +2,20 @@ require 'spec_helper'
 require 'rspec_api_documentation/dsl'
 
 resource 'Mentor' do
-  header "Accept", "application/json"
-  header "Content-Type", "application/json"
+  header "Accept", "application/vnd.shyne.v1"
+
+  before(:each) do
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
+  let(:mentor) { Mentor.create(FactoryGirl.attributes_for(:mentor)) }
 
   get "/api/mentors" do
-    parameter :featured, "Featured mentors or not"
+    parameter :featured, "Only list featured mentors"
 
     before do
-      2.times do |i|
-        mentor = FactoryGirl.attributes_for(:mentor)
-        Mentor.create(mentor)
+      10.times do |i|
+        Mentor.create(FactoryGirl.attributes_for(:mentor))
       end
     end
 
@@ -29,11 +33,60 @@ resource 'Mentor' do
   end
 
   post "/api/mentors" do
+    parameter :first_name, "First name", :required => true, :scope => :mentor
+    parameter :last_name, "Last name", :required => true, :scope => :mentor
+    parameter :headline, "Headline", :required => true, :scope => :mentor
+    parameter :years_of_experience, "Headline", :required => true, :scope => :mentor
+    parameter :phone_number, "Phone Number", :required => true, :scope => :mentor
+    parameter :availability, "Availability", :required => true, :scope => :mentor
+    parameter :email, "Email", :scope => :user
+    parameter :password, "Password", :scope => :user
+    parameter :password_confirmation, "Password Confirmation", :scope => :user
+
+    example "Registering a mentor" do
+      mentor = FactoryGirl.attributes_for(:mentor, :featured => nil).except(:id)
+      mentor[:user_attributes] = FactoryGirl.attributes_for(:user).except(:id)
+      do_request(mentor: mentor)
+
+      #TODO: Fix this
+      #response_body.should be_json_eql(mentor.except(:user_attributes).to_json)
+
+      status.should == 201
+    end
+  end
+
+  get "/api/mentors/:id" do
+    let(:id) { mentor.id }
+
+    example_request "Getting a specific mentor" do
+      response_body.should == mentor.to_json
+      status.should == 200
+    end
+  end
+
+  put "/api/mentors/:id" do
+    parameter :first_name, "First name", :required => true, :scope => :mentor
+    parameter :last_name, "Last name", :required => true, :scope => :mentor
+    parameter :headline, "Headline", :required => true, :scope => :mentor
+    parameter :years_of_experience, "Headline", :required => true, :scope => :mentor
+    parameter :phone_number, "Phone Number", :required => true, :scope => :mentor
+    parameter :availability, "Availability", :required => true, :scope => :mentor
+
+    let(:id) { mentor.id }
+
+    example "Updating a mentor" do
+      mentor = FactoryGirl.attributes_for(:mentor, :featured => nil).except(:id)
+      do_request(mentor: mentor)
+
+      status.should == 204
+    end
   end
 
   delete "/api/mentors/:id" do
-  end
+    let(:id) { mentor.id }
 
-  patch "/api/mentors/:id" do
+    example_request "Deleting a mentor" do
+      status.should == 204
+    end
   end
 end
--- spec/factories/mentors.rb
+++ spec/factories/mentors.rb
@@ -12,5 +12,10 @@ FactoryGirl.define do
   factory :mentor do
     first_name Faker::Name.first_name
     last_name Faker::Name.last_name
+    headline Faker::Lorem.sentence(10)
+    years_of_experience { rand(30) }
+    phone_number Faker::PhoneNumber.phone_number
+    availability Faker::Lorem.sentence(10)
+    featured [true, false].sample
   end
 end
