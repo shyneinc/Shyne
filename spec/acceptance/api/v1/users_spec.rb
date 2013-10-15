dee574e82c7f588613df57aff13b8fbb743ddd82
jesalg <jesal7@gmail.com>
2013-10-14 17:14:14 -0700

    User spec

---
+++ spec/acceptance/api/v1/users_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+require 'rspec_api_documentation/dsl'
+
+resource 'User' do
+  header "Accept", "application/vnd.shyne.v1"
+
+  post "/api/users" do
+    parameter :email, "First name", :required => true, :scope => :user
+    parameter :password, "Last name", :required => true, :scope => :user
+    parameter :password_confirmation, "Last name", :required => true, :scope => :user
+
+    example "Registering a user" do
+      user = FactoryGirl.attributes_for(:user).except(:id)
+      do_request(user: user)
+
+      hash = JSON.parse(response_body)
+      hash.delete('role_id')
+      hash.delete('role_type')
+
+      hash.to_json.should be_json_eql(user.except(:password, :password_confirmation).to_json)
+
+      status.should == 201
+    end
+  end
+
+  put "/api/users" do
+    include Warden::Test::Helpers
+
+    before do
+      user = FactoryGirl.create(:user)
+      login_as user, scope: :user
+    end
+
+    parameter :email, "First name", :required => true, :scope => :user
+    parameter :password, "Last name", :required => true, :scope => :user
+    parameter :password_confirmation, "Last name", :required => true, :scope => :user
+
+    example "Updating a user" do
+      user_attrs = FactoryGirl.attributes_for(:user).except(:id)
+      do_request(user: user_attrs)
+
+      status.should == 204
+    end
+  end
+end
