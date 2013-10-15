45db90dd831c9af21d2887ad3dad07ddcba12d24
jesalg <jesal7@gmail.com>
2013-10-15 15:54:50 -0700

    Better test data

--- spec/factories/mentors.rb
+++ spec/factories/mentors.rb
@@ -13,7 +13,7 @@ FactoryGirl.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     headline { Faker::Lorem.sentence(10) }
-    experties '{Accounting,Finance}'
+    experties { ['{Accounting,Finance}','{Banking}','{Legal Services}','{Internet,E-learning}'].sample }
     years_of_experience { rand(30) }
     phone_number { Faker::PhoneNumber.phone_number }
     availability { Faker::Lorem.sentence(10) }
