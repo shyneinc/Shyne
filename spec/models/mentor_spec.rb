dfcb50adf31d5252faba5683bfeb46484094db91
jesalg <jesal7@gmail.com>
2013-10-16 15:17:26 -0700

    Adding test for mentor model

--- spec/models/mentor_spec.rb
+++ spec/models/mentor_spec.rb
@@ -5,5 +5,44 @@ describe Mentor do
     expect(build(:mentor)).to be_valid
   end
 
-  let(:mentor_instance) { create(:mentor) }
+  describe "ActiveModel validations" do
+    context "Basic validations" do
+      it { should validate_presence_of :first_name }
+      it { should validate_presence_of :last_name }
+      it { should validate_presence_of :headline }
+      it { should validate_presence_of :experties }
+      it { should validate_presence_of :years_of_experience }
+      it { should validate_presence_of :availability }
+      it { should validate_presence_of :phone_number }
+    end
+  end
+
+  it "returns a mentor's full name as a string" do
+    mentor = build_stubbed(:mentor, first_name: "Jane", last_name: "Doe")
+    expect(mentor.display_name).to eq "Jane Doe"
+  end
+
+  describe "filter by featured mentors" do
+    before :all do
+      @mentor1 = create(:mentor, featured: true)
+      @mentor2 = create(:mentor, featured: false)
+      @mentor3 = create(:mentor, featured: true)
+    end
+
+    context "matching mentors" do
+      it "returns an array of results that match" do
+        expect(Mentor.featured).to eq [@mentor1, @mentor3]
+      end
+    end
+
+    context "non-matching mentors" do
+      it "returns an array of results that match" do
+        expect(Mentor.featured).to_not include @mentor2
+      end
+    end
+
+    after :all do
+      Mentor.delete_all
+    end
+  end
 end
