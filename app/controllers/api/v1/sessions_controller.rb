2f0bd2c09f121c6bc11332e5427f525ebd378056
jesalg <jesal7@gmail.com>
2013-10-17 23:00:33 -0700

    Adding confirmable feature & refactoring Session/Users controllers

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,37 +2,17 @@ class Api::V1::SessionsController < Devise::SessionsController
     protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/vnd.shyne.v1' }
 
     def create
-      resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
-      render :status => 200,
-             :json => { :success => true,
-                        :info => "Logged in",
-                        :user => current_user
-             }
+      warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+      render :json => { :info => "Logged in", :user => current_user }, :status => 200
     end
 
     def destroy
       warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
       sign_out
-      render :status => 200,
-             :json => { :success => true,
-                        :info => "Logged out",
-             }
+      render :json => { :info => "Logged out" }, :status => 200
     end
 
     def failure
-      render :status => 401,
-             :json => { :success => false,
-                        :info => "Login Credentials Failed"
-             }
-    end
-
-    def show_current_user
-      warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
-      render :status => 200,
-             :json => { :success => true,
-                        :info => "Current User",
-                        :user => current_user
-             }
-
+      render :json => { :error => "Login Credentials Failed" }, :status => 401
     end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,28 @@
 class Api::V1::UsersController < Api::V1::BaseController
   before_filter :authenticate_user!, :except => [:create]
 
+  def show
+    render :json => { :info => "Current User", :user => current_user, :confirmed => current_user.confirmed? }, :status => 200
+  end
+
   def create
     @user = User.create(user_params)
     sign_in(@user)
-    respond_with @user, :location => api_show_current_user_path
+    respond_with @user, :location => api_users_path
   end
 
   def update
     respond_with :api, User.update(current_user.id, user_params)
+  end
+
+  def confirm
+    @user = User.confirm_by_token(params[:confirmation_token])
+    if @user.errors.empty?
+      sign_in(@user)
+      render :json => { :confirmed => @user.confirmed? }, :status => 200
+    else
+      render :json => { :error => 'Confirmation token is invalid' }, :status => 400
+    end
   end
 
   private
