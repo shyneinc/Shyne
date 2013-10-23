c2fb20aae3d3560fb4167c85a62e3070178933b1
jesalg <jesal7@gmail.com>
2013-10-22 17:52:02 -0700

    Disable image processing for test environment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,16 @@
 CarrierWave.configure do |config|
-  	if Rails.env.production?
-  		config.storage = :fog
-		config.fog_credentials = {
-			:provider               => "AWS",
-		    :aws_access_key_id      => ENV['aws_access_key'],
-		    :aws_secret_access_key  => ENV['aws_secret_key']
-		}
-		config.fog_directory  = ENV['aws_bucket']
-
-		config.fog_public = false
-		config.fog_authenticated_url_expiration = 600 # 10 minutes
-	else
-    	config.storage = :file
-  	end
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_credentials = {
+        :provider => "AWS",
+        :aws_access_key_id => ENV['aws_access_key'],
+        :aws_secret_access_key => ENV['aws_secret_key']
+    }
+    config.fog_directory = ENV['aws_bucket']
+  elsif Rails.env.development?
+    config.storage = :file
+  else
+    config.storage = :file
+    config.enable_processing = false
+  end
 end
