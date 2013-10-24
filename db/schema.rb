beb302d05b0ebf9e23948b90525fa9b555e4ee34
audeemac <amvelasco@gmail.com>
2013-10-24 15:45:26 -0700

    migration - adding username to User Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131024204818) do
+ActiveRecord::Schema.define(version: 20131024224142) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20131024204818) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "avatar"
+    t.string   "username"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
