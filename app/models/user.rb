293a48fec9d474772d06027919bb7a7f5d9aef9f
audeemac <amvelasco@gmail.com>
2013-10-24 16:55:39 -0700

    generate username from first_name and last_name of user model [7440598]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+  after_validation :generate_username
+
   validates :first_name, :last_name, presence: true
 
   mount_uploader :avatar, AvatarUploader
@@ -13,4 +15,13 @@ class User < ActiveRecord::Base
   def active_for_authentication?
     true
   end
+
+  private
+    def generate_username
+      tmp_username = (self.first_name + self.last_name).gsub(/\s+/, "").to_s.downcase
+      iterator = User.where("username like ?", "%#{tmp_username}%").pluck(:username).count    
+      tmp_username += iterator.to_s if iterator > 0 #append count where there are similar usernames
+      
+      self.username = tmp_username
+    end
 end
