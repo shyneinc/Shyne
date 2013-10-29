fc263e4533e0891ef98afe1a40cc869cb4b0f8b0
audeemac <amvelasco@gmail.com>
2013-10-29 16:36:53 -0700

    destroying call_meter and adding call_status model

--- db/structure.sql
+++ db/structure.sql
@@ -121,6 +121,37 @@ ALTER SEQUENCE admins_id_seq OWNED BY admins.id;
 
 
 --
+-- Name: call_statuses; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE call_statuses (
+    id integer NOT NULL,
+    type character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+--
+-- Name: call_statuses_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE call_statuses_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: call_statuses_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE call_statuses_id_seq OWNED BY call_statuses.id;
+
+
+--
 -- Name: calls; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -468,6 +499,13 @@ ALTER TABLE ONLY admins ALTER COLUMN id SET DEFAULT nextval('admins_id_seq'::reg
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
+ALTER TABLE ONLY call_statuses ALTER COLUMN id SET DEFAULT nextval('call_statuses_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
 ALTER TABLE ONLY calls ALTER COLUMN id SET DEFAULT nextval('calls_id_seq'::regclass);
 
 
@@ -541,6 +579,14 @@ ALTER TABLE ONLY active_admin_comments
 
 ALTER TABLE ONLY admins
     ADD CONSTRAINT admins_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: call_statuses_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY call_statuses
+    ADD CONSTRAINT call_statuses_pkey PRIMARY KEY (id);
 
 
 --
@@ -834,6 +880,8 @@ INSERT INTO schema_migrations (version) VALUES ('20131024204818');
 INSERT INTO schema_migrations (version) VALUES ('20131024224142');
 
 INSERT INTO schema_migrations (version) VALUES ('20131025171144');
+
+INSERT INTO schema_migrations (version) VALUES ('20131029224057');
 
 INSERT INTO schema_migrations (version) VALUES ('20131030211013');
 
