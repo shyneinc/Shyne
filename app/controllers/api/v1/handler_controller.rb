1308f6ca9f104e4a72a0ee16e92b607fdcc6a3c8
audeemac <amvelasco@gmail.com>
2013-10-29 11:21:33 -0700

    need 'root_url' and changes to :get in routes

--- app/controllers/api/v1/handler_controller.rb
+++ app/controllers/api/v1/handler_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::HandlerController < ApplicationController
 
 	def index
 		@response = Twilio::TwiML::Response.new do |r|
-			r.Gather action: '/handler/conference', method: 'POST', numDigits: 5, timeout: 10 do |f|
+			r.Gather action: "#{root_url}handler/conference", method: 'GET', numDigits: 5, timeout: 10 do |f|
 				f.Say "Greetings! Welcome to Shyne!" , voice: 'alice'
 				f.Say "Enter Passcode:", voice: 'alice'
 			end
@@ -15,7 +15,7 @@ class Api::V1::HandlerController < ApplicationController
 	end
 
 	def conference
-		@user_input = params[:digits]
+		@user_input = params[:Digits]
 
 		@response = Twilio::TwiML::Response.new do |r|
 			r.Say "Entering the Dojo!", voice: 'alice'
