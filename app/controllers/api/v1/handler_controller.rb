f9f4f1c5362597936c82ff92b5c8dbbe08250dd5
audeemac <amvelasco@gmail.com>
2013-10-29 10:50:43 -0700

    making handler ask for a passcode first and then redirect to that passcode's conference room (no validation for now)

--- app/controllers/api/v1/handler_controller.rb
+++ app/controllers/api/v1/handler_controller.rb
@@ -5,12 +5,23 @@ class Api::V1::HandlerController < ApplicationController
 
 	def index
 		@response = Twilio::TwiML::Response.new do |r|
-			r.Say "Greetings! Welcome to Shyne Conference"
-			r.Dial do |f|
-				f.Conference "Room 808"
+			r.Gather action: '/handler/conference', method: 'POST', numDigits: 5, timeout: 10 do |f|
+				f.Say "Greetings! Welcome to Shyne!" , voice: 'alice'
+				f.Say "Enter Passcode:", voice: 'alice'
 			end
 		end
 
 		render :xml => Nokogiri::XML(@response.text)
+	end
+
+	def conference
+		@user_input = params[:digits]
+
+		@response = Twilio::TwiML::Response.new do |r|
+			r.Say "Entering the Dojo!", voice: 'alice'
+			r.Dial do |d|
+				d.Conference @user_input
+			end
+		end
 	end
 end
