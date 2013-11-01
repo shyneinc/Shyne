586815e1d0c5887e2a0d03d8ad4713b23d0ceba9
audeemac <amvelasco@gmail.com>
2013-11-01 00:50:42 -0700

    generate random 5 digits for passcode

--- app/models/call.rb
+++ app/models/call.rb
@@ -2,7 +2,16 @@ class Call < ActiveRecord::Base
   belongs_to :member
   belongs_to :mentor
 
+  after_validation :generate_passcode, :on => :create
+
   has_many :reviews
 
   just_define_datetime_picker :scheduled_at
+
+  private
+    def generate_passcode
+      #should check for uniqueness
+      #smallint have 32,767 limit
+      self.passcode = Random.new.rand(10_000..32_767-1) 
+    end
 end
