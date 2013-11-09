6766f56d1e6a64265bd75e0e935b0c4e6e4528a6
jesalg <jesal7@gmail.com>
2013-11-08 22:46:11 -0800

    Adding model validation & tests for CallRequest

--- app/models/call_request.rb
+++ app/models/call_request.rb
@@ -1,4 +1,6 @@
 class CallRequest < ActiveRecord::Base
+  validates :status, :scheduled_at, :mentor, :member, presence: true
+
   classy_enum_attr :status, default: :proposed, enum: :call_request_status
 
   belongs_to :member
@@ -7,27 +9,27 @@ class CallRequest < ActiveRecord::Base
 
   after_validation :generate_passcode, :on => :create
   after_update :send_status_update, :if => :status_changed?
- 
+
   just_define_datetime_picker :scheduled_at
 
   private
-    def generate_passcode
-      #smallint have 32,767 limit and checked for uniqueness
-      begin
-        tmp_passcode = Random.new.rand(10_000..99_999) 
-      end while CallRequest.find_by passcode: tmp_passcode
-        
-      self.passcode = tmp_passcode
-    end
 
-    def send_status_update
-      if self.status.proposed?
-        puts 'send proposed sked' #need mailer and sms
-      elsif self.status.scheduled?
-        puts 'send sked confirmation' #need mailer and sms
-      elsif self.status.rescheduled?
-        puts 'send sked retry' #need mailer and sms
-      end   
+  def generate_passcode
+    begin
+      tmp_passcode = Random.new.rand(10_000..99_999)
+    end while CallRequest.find_by passcode: tmp_passcode
+    self.passcode = tmp_passcode
+  end
+
+  def send_status_update
+    if self.status.proposed?
+      puts 'send proposed sked' #need mailer and sms
+    elsif self.status.scheduled?
+      puts 'send sked confirmation' #need mailer and sms
+    elsif self.status.rescheduled?
+      puts 'send sked retry' #need mailer and sms
     end
-    handle_asynchronously :send_status_update ,:priority => 20
+  end
+
+  handle_asynchronously :send_status_update, :priority => 20
 end
--- spec/models/call_request_spec.rb
+++ spec/models/call_request_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe CallRequest do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(build(:call_request)).to be_valid
+  end
+
+  let(:call_request) { create(:call_request) }
+
+  describe "ActiveModel validations" do
+    context "Basic validations" do
+      it { should validate_presence_of :scheduled_at }
+      it { should validate_presence_of :mentor }
+      it { should validate_presence_of :member }
+    end
+  end
+
+  describe "ActiveRecord validations" do
+    context "Associations" do
+      it { expect(call_request).to belong_to(:mentor) }
+      it { expect(call_request).to belong_to(:member) }
+      it { expect(call_request).to have_many(:calls) }
+    end
+
+    context "Callbacks" do
+      it { expect(call_request).to callback(:generate_passcode).after(:validation).on(:create) }
+      it { expect(call_request).to callback(:send_status_update).after(:update) }
+    end
+  end
 end
