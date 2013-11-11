b1b6f03d6629a04dd828463f1a1d60c7605a6d88
jesalg <jesal7@gmail.com>
2013-11-11 14:11:28 -0800

    Require admin auth to view API docs

--- config/routes.rb
+++ config/routes.rb
@@ -4,61 +4,11 @@ Shyne::Application.routes.draw do
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get '/admin', to: redirect('/active_admin')
-  mount Raddocs::App => "/docs"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  authenticate(:admin) do
+    mount Raddocs::App => "/docs"
+  end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
   resources :home, only: [:index]
   root :to => "home#index"
   devise_for :users
@@ -104,7 +54,6 @@ Shyne::Application.routes.draw do
       match '/call/finish' => 'call#finish' , via: :post, defaults: {format: :xml}
     end
   end
-
 
 end
 
