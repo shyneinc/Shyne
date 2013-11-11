1fa2fb870e9d13f9aaf035d77b15237bd95bf835
audeemac <amvelasco@gmail.com>
2013-11-10 16:34:47 -0800

    adding call factories

--- spec/factories/call_requests.rb
+++ spec/factories/call_requests.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :call_request do
-    passcode { Random.new.rand(10_000..32_767-1) }
+    passcode { Random.new.rand(10_000..99_999) }
     status CallRequestStatus::Proposed.new
     scheduled_at { rand(30.days).from_now.to_s(:db) }
     member
--- spec/factories/calls.rb
+++ spec/factories/calls.rb
@@ -2,5 +2,11 @@
 
 FactoryGirl.define do
   factory :call do
+  	from_number { Faker::PhoneNumber.phone_number.split('x').first }
+  	status { CallStatus::Inprogress.new }
+  	billed false
+  	conferencesid "CF0ce8cba9ba6648cc81e61622b1d8ab93"
+  	sid "CAc391fbdef33c33b5770507fb80b1472b"
+  	call_request
   end
 end
