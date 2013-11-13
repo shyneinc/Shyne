5b619a8834c3bb9e5b38c827fd80093731b7cc79
audeemac <amvelasco@gmail.com>
2013-11-13 14:30:14 -0800

    us_zones does not allow utc

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ describe User do
       it { should validate_presence_of :last_name }
       it { should validate_presence_of :email }
       it { should validate_presence_of :password }
-      it { should allow_value("UTC").for(:time_zone) }
+      it { should allow_value("Hawaii").for(:time_zone) }
       it { should_not allow_value("PST").for(:time_zone) }
     end
   end
