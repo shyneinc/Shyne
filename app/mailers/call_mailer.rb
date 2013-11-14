50c875536d13b5539e24549255ec90a449766949
audeemac <amvelasco@gmail.com>
2013-11-13 17:23:15 -0800

    CallMailer templates

--- app/mailers/call_mailer.rb
+++ app/mailers/call_mailer.rb
@@ -1,19 +1,45 @@
 class CallMailer < ActionMailer::Base
   default from: "no-reply@shyne.io"
 
-  def appointment_approved
-    
+  def request_proposed(member, mentor, call_request)
+    @member = member
+    @mentor = mentor
+    @call_request = call_request
+    mail(to: mentor.user.email, cc: member.user.email, subject: "Call Request have been proposed!" )
   end
 
-  def appointment_changed
-    
+  def request_approved(member, mentor, call_request)
+    @member = member
+    @mentor = mentor
+    @call_request = call_request
+    mail(to: member.user.email, cc: mentor.user.email, subject: "Call Request have been approved!" )
   end
 
-  def send_reminder
-    
+  def request_change_proposed(member, mentor, call_request)
+    @member = member
+    @mentor = mentor
+    @call_request = call_request
+    mail(to: member.user.email, cc: mentor.user.email, subject: "Schedule change has been proposed!" )
   end
 
-  def send_bill
-    
+  def request_change_approved(member, mentor, call_request)
+    @member = member
+    @mentor = mentor
+    @call_request = call_request
+    mail(to: mentor.user.email, cc: member.user.email, subject: "Schedule change has been approved!" )
+  end
+
+  def send_reminder(member, mentor, call_request)
+    @member = member
+    @mentor = mentor
+    @call_request = call_request
+    mail(to: mentor.user.email, cc: member.user.email, subject: "Call Reuest have been proposed!" )
+  end
+
+  def send_duration(member, mentor, call)
+    @member = member
+    @mentor = mentor
+    @call = call
+    mail(to: mentor.user.email, cc: member.user.email, subject: "Call Reuest have been proposed!" )
   end
 end
