9e91a85c3f26cc5472f732ff4791d88eb5bcf1ea
jesalg <jesal7@gmail.com>
2013-11-13 23:41:45 -0800

    Also need this in test env?

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ Shyne::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Workaround for "Error dumping database" error on Heroku
+  config.active_record.schema_format = :ruby
 end
