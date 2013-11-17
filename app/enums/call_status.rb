0891bfce30e29f8dc987b2a0efa5d6409f3e9bf4
jesalg <jesal7@gmail.com>
2013-11-17 12:08:00 -0800

    Follow single responsibility principle. Only thing this should do is get the call duration of this particular call instance.

--- app/enums/call_status.rb
+++ app/enums/call_status.rb
@@ -8,34 +8,11 @@ class CallStatus::Inprogress < CallStatus
 end
 
 class CallStatus::Completed < CallStatus
-  # TODO
-  # Turn this into a rake task run it with whenever.
-  # Seperate task to calculate duration and to do billing
-  # Try alternate approach to calculate duration, where
-  # its conference.start - conference.end
   def fetch_duration
     if owner.sid
       @client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
       @log = @client.account.calls.get(owner.sid.to_s)
       owner.duration = @log.duration.to_i
-
-      if owner.save
-        @conf = @client.account.conferences.get(owner.conferencesid.to_s)
-        callers = Call.where(conferencesid: @conf.sid , status: :completed).count
-        
-        if @conf && @conf.status == 'completed' && callers > 1 
-          @duration = @conf.date_updated.to_time - @conf.date_created.to_time
-          
-          @call_request = owner.call_request
-          @call_request.billable_duration = @duration
-          @call_request.conferencesid = @conf.sid
-
-          if @call_request.save
-            CallMailer.delay.send_duration( @call_request )
-          end
-        end
-      end
-      
     end
   end
   handle_asynchronously :fetch_duration, :run_at => Proc.new { 3.minutes.from_now }
