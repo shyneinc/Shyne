f81b5f73f37678d2616735b04d5d7bc9111abf3a
jesalg <jesal7@gmail.com>
2013-11-18 10:52:40 -0800

    Remove passcode after duration has been calculated

--- app/models/call_request.rb
+++ app/models/call_request.rb
@@ -28,6 +28,7 @@ class CallRequest < ActiveRecord::Base
           end
         end
         self.billable_duration = @call_durations.values.min
+        self.passcode = -1 if self.billable_duration > 0
         if self.save
           CallRequestMailer.delay.send_duration(self)
         end
