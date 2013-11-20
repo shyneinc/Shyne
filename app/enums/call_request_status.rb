d068d14dd6d833709492801637315af2e8c663ea
audeemac <amvelasco@gmail.com>
2013-11-19 16:57:43 -0800

    scheduling approval status mail

--- app/enums/call_request_status.rb
+++ app/enums/call_request_status.rb
@@ -12,7 +12,21 @@ end
 
 class CallRequestStatus::Approved < CallRequestStatus
   def send_status
-    CallRequestMailer.delay.request_approved(owner)
+    12_hours = 12.hours.from_now
+      4_hours = 4.hours.from_now
+      1_hour = 1.hour.from_now
+      sched = owner.scheduled_at
+
+    if sched > 12_hours
+      # :run_at => Proc.new { 12.hours.from_now }
+      CallRequestMailer.delay(run_at: 12.hours.from_now).request_approved(owner)
+    elsif sched < 12_hours && sched > 4_hours
+      CallRequestMailer.delay(run_at: 4.houts.from_now).request_approved(owner)
+    elsif sched < 4_hours && sched > 1_hour
+      CallRequestMailer.delay(run_at: 1.hour.from_now).request_approved(owner)
+    else
+      CallRequestMailer.request_approved(owner)
+    end
   end
 end
 
