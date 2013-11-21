290f94678c67f59188084bdb9b74a924e201e600
jesalg <jesal7@gmail.com>
2013-11-20 17:17:13 -0800

    Fixing failing build

--- app/enums/call_request_status.rb
+++ app/enums/call_request_status.rb
@@ -12,17 +12,17 @@ end
 
 class CallRequestStatus::Approved < CallRequestStatus
   def send_status
-    12_hours = 12.hours.from_now
-      4_hours = 4.hours.from_now
-      1_hour = 1.hour.from_now
-      sched = owner.scheduled_at
+    twelve_hours = 12.hours.from_now
+    four_hours = 4.hours.from_now
+    one_hour = 1.hour.from_now
+    sched = owner.scheduled_at
 
-    if sched > 12_hours
+    if sched > twelve_hours
       # :run_at => Proc.new { 12.hours.from_now }
       CallRequestMailer.delay(run_at: 12.hours.from_now).request_approved(owner)
-    elsif sched < 12_hours && sched > 4_hours
+    elsif sched < twelve_hours && sched > four_hours
       CallRequestMailer.delay(run_at: 4.houts.from_now).request_approved(owner)
-    elsif sched < 4_hours && sched > 1_hour
+    elsif sched < four_hours && sched > one_hour
       CallRequestMailer.delay(run_at: 1.hour.from_now).request_approved(owner)
     else
       CallRequestMailer.request_approved(owner)
