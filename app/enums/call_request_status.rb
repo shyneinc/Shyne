9d8e92f2fd4c62723c33d45e51ae2dc0b75084bb
jesalg <jesal7@gmail.com>
2013-11-21 17:08:46 -0800

    Need to rethink this logic or utilize icalendar reminders

--- app/enums/call_request_status.rb
+++ app/enums/call_request_status.rb
@@ -14,25 +14,6 @@ class CallRequestStatus::Proposed < CallRequestStatus
 end
 
 class CallRequestStatus::Approved < CallRequestStatus
-  #schdule reminder
-
-  # def send_status
-  #   twelve_hours = 12.hours.from_now
-  #   four_hours = 4.hours.from_now
-  #   one_hour = 1.hour.from_now
-  #   sched = owner.scheduled_at
-
-  #   if sched > twelve_hours
-  #     # :run_at => Proc.new { 12.hours.from_now }
-  #     CallRequestMailer.delay(run_at: 12.hours.from_now).request_approved(owner)
-  #   elsif sched < twelve_hours && sched > four_hours
-  #     CallRequestMailer.delay(run_at: 4.houts.from_now).request_approved(owner)
-  #   elsif sched < four_hours && sched > one_hour
-  #     CallRequestMailer.delay(run_at: 1.hour.from_now).request_approved(owner)
-  #   else
-  #     CallRequestMailer.request_approved(owner)
-  #   end
-  # end
   def send_status
     CallRequestMailer.delay.request_approved(owner)
     Sms.delay.send("Your call request is approved!" , owner.member.phone_number.to_s )
--- lib/tasks/call_request.rake
+++ lib/tasks/call_request.rake
@@ -5,4 +5,20 @@ namespace :call_request  do
       call_request.calculate_billable_duration
     end
   end
+
+  task :send_reminders => :environment do
+    CallRequest.where(billable_duration: nil, status: :approved).find_each do |call_request|
+=begin
+      if call_request.scheduled_at > 12.hours.from_now
+        CallRequestMailer.delay.remind(call_request)
+      elsif call_request.scheduled_at < 12.hours.from_now && owner.scheduled_at > 4.hours.from_now
+        CallRequestMailer.delay.remind(call_request)
+      elsif call_request.scheduled_at < 4.hours.from_now && owner.scheduled_at > 1.hour.from_now
+        CallRequestMailer.delay.remind(call_request)
+      else
+        CallRequestMailer.remind(owner)
+      end
+=end
+    end
+  end
 end
