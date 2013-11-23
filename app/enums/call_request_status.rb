5be807f837e3e6f9e110d8e3ecaa555c33c1b9b0
audeemac <amvelasco@gmail.com>
2013-11-22 17:05:01 -0800

    namespaced concern under twilio

--- app/enums/call_request_status.rb
+++ app/enums/call_request_status.rb
@@ -1,5 +1,5 @@
 class CallRequestStatus < ClassyEnum::Base
-  include Sms
+  include Twilio::Sms
   
   def send_status
     false
@@ -9,28 +9,28 @@ end
 class CallRequestStatus::Proposed < CallRequestStatus
   def send_status
     CallRequestMailer.delay.request_proposed(owner)
-    Sms.delay.send("Your have call request!" , owner.mentor.phone_number.to_s )
+    Twilio::Sms.delay.send("Your have call request!" , owner.mentor.phone_number.to_s )
   end
 end
 
 class CallRequestStatus::Approved < CallRequestStatus
   def send_status
     CallRequestMailer.delay.request_approved(owner)
-    Sms.delay.send("Your call request is approved!" , owner.member.phone_number.to_s )
+    Twilio::Sms.delay.send("Your call request is approved!" , owner.member.phone_number.to_s )
   end
 end
 
 class CallRequestStatus::Changed < CallRequestStatus
   def send_status
     CallRequestMailer.delay.request_changed(owner)
-    Sms.delay.send("Your call request is approved!" , owner.member.phone_number.to_s )
-    Sms.delay.send("Your call request is approved!" , owner.mentor.phone_number.to_s )
+    Twilio::Sms.delay.send("Your call request is approved!" , owner.member.phone_number.to_s )
+    Twilio::Sms.delay.send("Your call request is approved!" , owner.mentor.phone_number.to_s )
   end
 end
 
 class CallRequestStatus::Completed < CallRequestStatus
   def send_status
     CallRequestMailer.delay.request_completed(owner)
-    Sms.delay.send("Your call is completed!" , owner.member.phone_number.to_s )
+    Twilio::Sms.delay.send("Your call is completed!" , owner.member.phone_number.to_s )
   end
 end
---
+++ app/models/concerns/twilio/sms.rb
@@ -0,0 +1,32 @@
+module Twilio::Sms
+  extend ActiveSupport::Concern
+
+  @client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
+
+  def self.send(message , to_number )
+    if @client
+      res = @client.account.sms.messages.create({
+              :to => "#{to_number}",
+              :from => "#{ENV['TWILIO_NUMBER']}",
+              :body => "#{message}"
+            })
+      return true unless res == nil
+    end
+  end
+
+  def  self.send_approval(to_number)
+    if @client
+      res = @client.account.sms.messages.create({
+              :to => "#{to_number}",
+              :from => "#{ENV['TWILIO_NUMBER']}",
+              :body => "Your call request has been approved!"
+            })
+      return true unless res == nil
+    end
+  end
+
+  def self.send_request(to_number)
+    return "TODO: send a request sms to mentor"
+  end
+  #should create canned messages here for approval .etc
+end
