094c0d0b41027c63cbc16bc34f4fef335f37090b
jesalg <jesal7@gmail.com>
2013-11-24 13:32:11 -0800

    Models can inherit from a table through the attribute :type, setting the inheritance_column to nil removes that attribute allowing you to have a database column named type

--- app/models/payment_transaction.rb
+++ app/models/payment_transaction.rb
@@ -2,4 +2,6 @@ class PaymentTransaction < ActiveRecord::Base
   validates :call_request, :type, :amount, :status, :uri, presence: true
 
   belongs_to :call_request
+
+  self.inheritance_column = nil
 end
