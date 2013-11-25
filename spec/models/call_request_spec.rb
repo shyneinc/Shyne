048975d9124c3b49c6f96b4697074b6079b201ac
Jesal Gadhia <jesal7@gmail.com>
2013-11-24 22:21:56 -0800

    Stubs for more test coverage

--- spec/models/call_request_spec.rb
+++ spec/models/call_request_spec.rb
@@ -20,11 +20,43 @@ describe CallRequest do
       it { expect(call_request).to belong_to(:mentor) }
       it { expect(call_request).to belong_to(:member) }
       it { expect(call_request).to have_many(:calls) }
+      it { expect(call_request).to have_many(:payment_transactions) }
     end
 
     context "Callbacks" do
       it { expect(call_request).to callback(:generate_passcode).after(:validation).on(:create) }
       it { expect(call_request).to callback(:send_status).after(:update) }
+      it { expect(call_request).to callback(:calc_mentor_duration).after(:update) }
+    end
+  end
+
+  describe "Public class methods" do
+    context "#calculate_billable_duration" do
+      pending
+    end
+
+    context "#process_payment" do
+      pending
+    end
+
+    context "#member_debited?" do
+      pending
+    end
+
+    context "#mentor_credited?" do
+      pending
+    end
+
+    context "#debit_amount" do
+      pending
+    end
+
+    context "#credit_amount" do
+      pending
+    end
+
+    context "#description" do
+      pending
     end
   end
 end
