e487a6bcd491dfa8ac5651fa5b1d32a54a931ffc
audeemac <amvelasco@gmail.com>
2013-12-04 15:31:26 -0800

    adding location/where field on google calendar

--- app/models/concerns/ical/reminder.rb
+++ app/models/concerns/ical/reminder.rb
@@ -11,6 +11,7 @@ module Ical::Reminder
         dtstart     date.utc 
         dtend       date.utc
         summary     "Scheduled call with #{options[:mentor]}"
+        location    "+1(909)480-4755 Passcode:#{options[:passcode]}"
         description "Shyne: you have a scheduled call with #{options[:mentor]} at #{date.to_s(:long)}. Passcode: #{options[:passcode]}"
         klass       "PRIVATE"
 
