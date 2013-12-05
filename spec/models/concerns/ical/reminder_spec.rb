7dcde9d99167bb98d6190ae06798feae32360de6
audeemac <amvelasco@gmail.com>
2013-12-04 17:18:12 -0800

    spec for Ical::Reminder

---
+++ spec/models/concerns/ical/reminder_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'rspec_api_documentation/dsl'
+
+describe "ReminderSpec" do
+	let(:call_request) { create(:call_request) }
+	describe "#post" do
+		it "returns an object" do
+			expect(Ical::Reminder.post({passcode: call_request.passcode, mentor: call_request.mentor.full_name, date: call_request.scheduled_at }, call_request.member.email )).to_not eql nil
+	  end
+	  it "return an ical class" do
+	  	expect(Ical::Reminder.post({passcode: call_request.passcode, mentor: call_request.mentor.full_name, date: call_request.scheduled_at }, call_request.member.email ).class).to eql String
+	  end
+	  it "return nil if no email(2nd argument)" do 
+	  	expect(Ical::Reminder.post({passcode: call_request.passcode, mentor: call_request.mentor.full_name, date: call_request.scheduled_at })).to eql nil
+	  end
+	  it "return nil if no options(1st argument)" do 
+	  	expect(Ical::Reminder.post( call_request.member.email )).to eql nil
+	  end
+	end
+end
