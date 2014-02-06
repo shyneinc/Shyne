81c1e8cfe16275b9c953a1872dcaedf3b76f6cb2
Jesal Gadhia <jesal7@gmail.com>
2014-02-06 08:24:27 -0800

    TODO: Cleanup industry endpoint

--- app/controllers/api/v1/industries_controller.rb
+++ app/controllers/api/v1/industries_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::IndustriesController < Api::V1::BaseController
   def index
+    # TODO: Need to clean this up, do something like:
+    # respond_with :api, Industry.select(:id, :title).map{ |i| [i.id, i.title.html_safe] }
     industry_list = []
     industries = Industry.select(:id, :title).where("title LIKE ?", "%#{params[:query]}%")
     industries.each do |industry|
