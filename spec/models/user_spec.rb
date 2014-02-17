73a31e004c92e8c1a8cc7dddd68a030d0200c02a
jesalg <jesal7@gmail.com>
2014-02-17 09:38:03 -0800

    Adding rspec test coverage for email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ describe User do
       it { should validate_presence_of :first_name }
       it { should validate_presence_of :last_name }
       it { should validate_presence_of :email }
+      it { should validate_uniqueness_of :email }
       it { should validate_presence_of :password }
       it { should allow_value("Hawaii").for(:time_zone) }
       it { should_not allow_value("PST").for(:time_zone) }
