f827e4bd6ed23b9630b2e2f2dcc8453b54c061e3
jesalg <jesal7@gmail.com>
2014-02-24 09:21:02 -0800

    Bug fix

--- spec/acceptance/api/v1/call_requests_spec.rb
+++ spec/acceptance/api/v1/call_requests_spec.rb
@@ -18,7 +18,7 @@ resource 'CallRequest' do
   get "/api/call_requests" do
     example_request "Getting all call requests" do
       role = user.role_type.downcase
-      expect(response_body).to be_json_eql CallRequest.where("#{role}_id" => current_user.role_id).to_json({:include =>
+      expect(response_body).to be_json_eql CallRequest.where("#{role}_id" => user.role_id).to_json({:include =>
                                                                                                                 {:mentor =>
                                                                                                                      {:methods => [:full_name, :rate_per_minute, :phone_number, :avg_call_duration, :get_avg_rating]},
                                                                                                                  :member => {:methods => [:full_name, :phone_number]}
