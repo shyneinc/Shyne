03f4882d202ad0310bb54581cd0c2c9e21d58833
soft-dave <davejc99@gmail.com>
2014-04-10 16:17:49 +0530

    Added methods to display previous and total reviews in search result page

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::SearchController < Api::V1::BaseController
       results.each { |result| mentor_ids << result[:searchable_id] }
       if (mentor_ids.count > 0)
         mentors = Mentor.find(mentor_ids.split(','))
-        respond_with :api, mentors.to_json(:only => [:id, :user_id, :city, :state, :headline, :years_of_experience, :full_address, :rate_per_minute, :city, :state], :methods => [:current_position, :current_company, :full_name, :photo_url, :rate_per_minute, :get_avg_rating, :currently_working_at, :previously_worked_at])
+        respond_with :api, mentors.to_json(:only => [:id, :user_id, :city, :state, :headline, :years_of_experience, :full_address, :rate_per_minute, :city, :state], :methods => [:current_position, :current_company, :full_name, :photo_url, :rate_per_minute, :get_avg_rating, :currently_working_at, :previously_worked_at, :previous_companies, :total_reviews])
       elsif render :json => {:info => 'No results found'}, :status => 200
       end
     else
