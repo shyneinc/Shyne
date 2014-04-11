4e54d217b4246db06aa1bbad39684b2eb2cf5ab2
jesalg <jesal7@gmail.com>
2014-04-10 17:04:42 -0700

    Spec update

--- spec/acceptance/api/v1/call_requests_spec.rb
+++ spec/acceptance/api/v1/call_requests_spec.rb
@@ -20,7 +20,7 @@ resource 'CallRequest' do
       role = user.role_type.downcase
       expect(response_body).to be_json_eql CallRequest.where("#{role}_id" => user.role_id).to_json({:include =>
                                                                                                                 {:mentor =>
-                                                                                                                     {:methods => [:full_name, :rate_per_minute, :phone_number, :avg_call_duration, :get_avg_rating]},
+                                                                                                                     {:methods => [:full_name, :rate_per_minute, :phone_number, :avg_call_duration, :get_avg_rating, :previous_companies, :current_position, :current_company, :total_reviews]},
                                                                                                                  :member => {:methods => [:full_name, :phone_number]}
                                                                                                                 }, :methods => [:scheduled_date, :credit_amount, :debit_amount]})
       expect(status).to eq 200
