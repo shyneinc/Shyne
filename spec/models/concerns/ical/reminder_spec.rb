dc6d6950937050bf7c9adc94bca7862c1bfda5e5
Jesal Gadhia <jesal7@gmail.com>
2014-05-12 22:17:38 -0700

    One more spec fix

--- spec/models/concerns/ical/reminder_spec.rb
+++ spec/models/concerns/ical/reminder_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'rspec_api_documentation/dsl'
 
 describe "ReminderSpec" do
-  let(:call) { Ical::Reminder.post({passcode: "12345", mentor: "John Doe", date: DateTime.now + 2.days}, "test@email.com") }
+  let(:call) { Ical::Reminder.post({passcode: "12345", guest: "John Doe", date: DateTime.now + 2.days}, "test@email.com") }
   describe "#post" do
     it "returns an object" do
       expect(call).to_not eql nil
@@ -24,7 +24,7 @@ describe "ReminderSpec" do
     end
     #i still need to test this because this is manually implemented and not coming from ruby
     it "raise error if wrong arguments" do
-      lambda { Ical::Reminder.post({passcode: "12345", mentor: "John Doe", date: DateTime.now}) }.should raise_error
+      lambda { Ical::Reminder.post({passcode: "12345", guest: "John Doe", date: DateTime.now}) }.should raise_error
     end
   end
 end
