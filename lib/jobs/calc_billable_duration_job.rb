c608d68dddeab1c2705b23e59f706a9622331057
Jesal Gadhia <jesal7@gmail.com>
2014-05-27 09:19:40 -0700

    Bug fix

--- lib/jobs/calc_billable_duration_job.rb
+++ lib/jobs/calc_billable_duration_job.rb
@@ -1,6 +1,6 @@
 class CalcBillableDurationJob
   def perform
-    CallRequest.where(billable_duration: nil, status: :approved).find_each do |call_request|
+    CallRequest.where(billable_duration: nil, status: [:approved_mentor, :approved_member]).find_each do |call_request|
       call_request.calculate_billable_duration
     end
   end
