2d297da7f52ffea9e955c02f4c80e856984b1162
Jesal Gadhia <jesal7@gmail.com>
2014-07-26 11:18:25 -0700

    Fixing spec

--- spec/models/concerns/ical/reminder_spec.rb
+++ spec/models/concerns/ical/reminder_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'rspec_api_documentation/dsl'
 
 describe "ReminderSpec" do
-  let(:call) { Ical::Reminder.post({passcode: "12345", guest: "John Doe", date: DateTime.now + 2.days}, "test@email.com") }
+  let(:call) { Ical::Reminder.post({passcode: "12345", timezone: "America/Chicago", guest: "John Doe", date: DateTime.now + 2.days}, "test@email.com") }
   describe "#post" do
     it "returns an object" do
       expect(call).to_not eql nil
